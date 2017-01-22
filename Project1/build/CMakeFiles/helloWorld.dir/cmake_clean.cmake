43931f747f8ed2276d5a82a999d80febe9f16002
Jing <lijing070769@hotmail.com>
2017-01-22 15:09:07 -0700

    first commit

---
+++ Project1/build/CMakeFiles/helloWorld.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/helloWorld.dir/src/main.cpp.o"
+  "helloWorld.pdb"
+  "helloWorld"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/helloWorld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Project1/src/main.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+#include <OpenGL/gl.h>
+#include <GLUT/glut.h>
+
+void onDisplay(){
+    static unsigned cnt = 0;
+    glClear(GL_COLOR_BUFFER_BIT);
+    if(cnt >= 30){
+        glClearColor((double)rand()/RAND_MAX,(double)rand()/RAND_MAX,(double)rand()/RAND_MAX,1.0f);
+        cnt = 0;
+    }
+    cnt++;
+    glutSwapBuffers();
+}
+
+void onKeyboard(unsigned char key, int x, int y){
+    if(key == 27){
+        exit(0);
+    }
+}
+
+
+void onIdle(){
+    glutPostRedisplay();
+}
+
+int main(int argc, char *argv[]){
+    srand(time(NULL));
+    glutInit(&argc, argv);
+
+    unsigned screen_width = 800;
+    unsigned screen_height = 600;
+    glutInitWindowSize(screen_width, screen_height);
+    glutCreateWindow("Project 1 of CS6610");
+
+    glutDisplayFunc(onDisplay);
+    glutKeyboardFunc(onKeyboard);
+    glutIdleFunc(onIdle);
+    glutMainLoop();
+}
